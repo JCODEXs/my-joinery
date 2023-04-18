5d051564146e434dfa1146529337c5aa00ef81c4
JCODEXs <juansebastianescobar.vega@gmail.com>
2023-04-18 12:17:31 -0500

    wip

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Maderafi</title>
-        <link rel="icon" href="favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
