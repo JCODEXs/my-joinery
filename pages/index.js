5c9be7cbbcab3ea7ad51ab76c76b7d38ff3077e2
JCODEXs <juansebastianescobar.vega@gmail.com>
2023-04-19 12:30:33 -0500

    wip

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import Gallery from "../components/Gallery/gallery";
 
 export default function Home() {
   return (
