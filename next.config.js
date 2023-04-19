7ef6550275805e470c6d695fb53939a0df885ff7
JCODEXs <juansebastianescobar.vega@gmail.com>
2023-04-19 12:53:36 -0500

    wip

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+ 
+    serverOptions: {
+      host: '0.0.0.0',
+      port: 3000,
+    
+  }
+  
 }
 
 module.exports = nextConfig
