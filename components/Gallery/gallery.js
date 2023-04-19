fac8f2c05f49fe8ebd86012927edd73456c1f24c
JCODEXs <juansebastianescobar.vega@gmail.com>
2023-04-19 13:51:20 -0500

    ready

--- components/Gallery/gallery.js
+++ components/Gallery/gallery.js
@@ -1,4 +1,4 @@
-import { memo } from "preact/compat";
+import { memo } from "react";
 import Story from "./story";
 export default memo(function Gallery({ stories, action = () => {} }) {
   return (
