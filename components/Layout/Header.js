b96ae44b1fefddc901ffc2c3aba064b0c3b94b37
JCODEXs <juansebastianescobar.vega@gmail.com>
2023-04-20 12:54:23 -0500

    version

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -1,12 +1,14 @@
 import { Fragment, useEffect, useState} from "react";
- import HeaderCartButton from "./HeaderCartButton";
+import HeaderCartButton from "./HeaderCartButton";
 import classes from "./Header.module.css";
 import Image from "next/image";
 
 
 
 const Header = (props) => {
- 
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+  
 
    const [Language,setLanguage] = useState('English')
 
@@ -44,18 +46,46 @@ useEffect(()=>{
       }
    
   }
-  
 
+  useEffect(() => {
+    function setDivHeight() {
+    const div = document.querySelector('#my-div');
+    const width = div.offsetWidth; 
+    const height = 100*1200 / width;
+   
+    setWidth(width);
+    setHeight(height);}
+    setDivHeight();
+
+    window.addEventListener('resize', setDivHeight); 
+  
+    return () => {
+      window.removeEventListener('resize', setDivHeight);
+    }
+
+  }, []);
+
+  useEffect(() => {
+    const div = document.querySelector('#my-div'); 
+    div.style.setProperty('height', `${height}px`); 
+   
+    }, [height]);
+
+ 
   return (
     <Fragment>
-      <header className={classes.header}>
-        <Image src="/MadeIn.png" alt="Vercel Logo" width={200} height={140} style={{borderRadius:"50%"}} />
-        
-          <button className={classes.button}   > Inicio </button>
-          <button className={classes.button}   > Productos </button>
-          <button className={classes.button}   > Talleres </button>
-          <button className={classes.button}   > Contacto </button>
-
+      <header id="my-div" className={classes.header}>
+        <div style={{borderRadius:"50%",zIndex:"11"}}>
+          <Image src="/MadeIn.png" alt="Vercel Logo" width={200} height={140} style={{borderRadius:"50%",zIndex:"9"}} />
+        </div>
+      
+          <div style={{borderRadius:"50%",minWidth:"10rem",maxWidth:"45rem",display:"flex",flexDirection:"row",flexWrap:"wrap"}}>
+            
+              <button className={classes.button}   > Inicio </button>
+              <button className={classes.button}   > Productos </button>
+              <button className={classes.button}   > Talleres </button>
+              <button className={classes.button}   > Contacto </button>
+          </div>
            {/* <button className={classes.button}  onClick ={enLanguageHandler} > {Language} </button> 
            */}
          {/* <Menubar className={classes.menubar}/> */}
